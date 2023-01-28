4fae546b3feb1cae4cfce7f88b8f014531a97da1
lukasz protosawicki <lukas.es.p@o2.pl>
2023-01-28 21:16:07 +0100

    add useState age + count2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const [age, setAge] = useState(42);
+	const [count2, setCount2] = useState(0);
+
+	useEffect(() => {
+		document.title = `Naciśnieto ${count2} razy`;
+	});
 
 	return (
 		<div className="App">
@@ -10,6 +16,15 @@ function App() {
 			<div>
 				<p>Naciśnięto {count} razy</p>
 				<button onClick={() => setCount(count + 1)}>Naciśnij mnie</button>
+			</div>
+			<div>
+				<p>Mam {age} lat</p>
+				<button onClick={() => setAge(age + 1)}>Zwiększ wiek</button>
+				<button onClick={() => setAge(age - 1)}>Zmniejsz wiek</button>
+			</div>
+			<div>
+				<p>Naciśnięto {count2} razy</p>
+				<button onClick={() => setCount2(count2 + 1)}>Naciśnij mnie</button>
 			</div>
 		</div>
 	);
